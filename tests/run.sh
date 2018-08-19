4aa4b6c9f15606366d35c452fdde5b85ad2b18db
Christopher Wells <cwells@anaxexp.io>
2018-08-18 21:10:40 -0700

    fixes

--- tests/run.sh
+++ tests/run.sh
@@ -9,21 +9,24 @@ fi
 name=$1
 image=$2
 
-cid="$(docker run -d --name "${name}" "${image}")"
-trap "docker rm -vf $cid > /dev/null" EXIT
+#cid="$(docker run -d --name "${name}" "${image}")"
+#trap "docker rm -vf $cid > /dev/null" EXIT
 
 opensmtpd() {
-	docker run --rm -i --link "${name}" "${image}" "${@}"
+	#docker run --rm -i --link "${name}" "${image}" "${@}"
+    docker-compose up -d
 }
 
 echo -n "Waiting for OpenSMTPD to start... "
-opensmtpd make check-ready host="${name}" max_try=10
+docker-compose exec opensmtpd make check-ready host="${name}" max_try=10
 echo "OK"
 
 echo -n "Checking OpenSMTPD version... "
-opensmtpd smtpd -h 2>&1 | grep -q "OpenSMTPD 6.0.*"
+docker-compose exec opensmtpd smtpd -h 2>&1 | grep -q "OpenSMTPD 6.0.*"
 echo "OK"
 
 echo -n "Validating OpenSMTPD config... "
-opensmtpd smtpd -n 2>&1 | grep -q "configuration OK"
+docker-compose exec opensmtpd smtpd -n 2>&1 | grep -q "configuration OK"
 echo "OK"
+
+docker-compose down
